ef4560e7a498d92d1a0ccba680f8957d8d3b536f
Emanuele-Paludetti <150333812+Emanuele-Paludetti@users.noreply.github.com>
2024-11-27 09:53:43 +0100

    Passo 9: add index generale

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'primo_progetto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates'),os.path.join(BASE_DIR, 'primo_progetto/templates')  ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
